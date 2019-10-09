5cc468d190b725b795741e4466d2200c211bbe2b
Roger <github@betechie.com>
2019-10-09 11:16:22 -0700

    initial commit of xamarin samples

---
+++ Okta.Xamarin.iOS.ExampleApp/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace Okta.Xamarin.iOS.ExampleApp
+{
+	public partial class ViewController : UIViewController
+	{
+
+		OidcClient client;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			OktaConfig config = OktaConfig.LoadFromPList("OktaConfig.plist");
+			this.configTestView.Text = $"Domain: {config.OktaDomain}\n" +
+				$"ClientId: {config.ClientId}\n" +
+				$"RedirectUri: {config.RedirectUri}\n" +
+				$"PostLogoutRedirecturi: {config.PostLogoutRedirectUri}\n" +
+				$"Scope: {string.Join(", ", config.Scopes)}\n" +
+				$"ClockSkew: {config.ClockSkew.ToString()}\n";
+
+			client = new OidcClient(this, config);
+
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		async partial void LoginBtn_TouchUpInside(UIButton sender)
+		{
+			var res = await client.SignInWithBrowserAsync();
+
+			this.configTestView.Text = $"AccessToken: {res.AccessToken}\n" +
+				$"IdToken: {res.IdToken}\n" +
+				$"Expires: {res.Expires}\n" +
+				$"Scope: {res.Scope}\n" +
+				$"RefreshToken: {res.RefreshToken ?? "<none>"}\n" +
+				$"TokenType: {res.TokenType}\n";
+
+			res.AccessToken.Clone();
+		}
+	}
+}
---
+++ Okta.Xamarin.iOS.ExampleApp/ViewController.designer.cs
@@ -0,0 +1,42 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Okta.Xamarin.iOS.ExampleApp
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView configTestView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton loginBtn { get; set; }
+
+        [Action ("LoginBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void LoginBtn_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (configTestView != null) {
+                configTestView.Dispose ();
+                configTestView = null;
+            }
+
+            if (loginBtn != null) {
+                loginBtn.Dispose ();
+                loginBtn = null;
+            }
+        }
+    }
+}
